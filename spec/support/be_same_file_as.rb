c72cf243589e78ac443fd9690d11e47a625c409c
Hot Cheese <mr.artur.su@gmail.com>
2016-11-21 19:51:35 +0100

    Update be_same_file_as.rb
    Fixed typo

--- spec/support/be_same_file_as.rb
+++ spec/support/be_same_file_as.rb
@@ -1,6 +1,6 @@
-RSpec::Matchers.define(:be_same_file_as) do |exected_file_path|
+RSpec::Matchers.define(:be_same_file_as) do |expected_file_path|
   match do |actual_file_path|
-    expect(md5_hash(actual_file_path)).to eq(md5_hash(exected_file_path))
+    expect(md5_hash(actual_file_path)).to eq(md5_hash(expected_file_path))
   end
 
   def md5_hash(file_path)
